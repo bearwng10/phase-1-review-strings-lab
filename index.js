1e3db704a8f14fdcfb4867e15ceb2a314e25fd4a
bearwng10 <96107954+bearwng10@users.noreply.github.com>
2023-06-06 11:38:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+// index.js
+
+// Define the currentUser variable with a string value
+const currentUser = 'John Doe';
+
+// Define the welcomeMessage variable using the currentUser variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable using the currentUser variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable using the currentUser variable
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
